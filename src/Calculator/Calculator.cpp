6926baedefb171995f3b7f83b40a04cf151312f9
Avishai Dernis <avid29@live.com>
2020-01-10 13:51:33 -0500

    Comments in main

--- src/Calculator/Calculator.cpp
+++ src/Calculator/Calculator.cpp
@@ -9,21 +9,25 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	// Get Equation
 	while (true) {
+		// Gets equation as string
 		string str;
 		cout << "Enter an Equation (q to quit): ";
 		cin >> str;
 
+		// Checks for 
 		if (str == "q") {
 			cout << "Done!";
 			return 0;
 		}
-
+		
+		// Convert equation to ExpTree
 		ExpTree *exp_tree = Parse(str);
 		if (exp_tree == nullptr) {
 			cout << "error occured" << endl;
 		}
+		
+		// Simplify ExpTree and conver back to string
 		cout << endl << exp_tree->Simplify()->Print() << endl;
 	}
 }

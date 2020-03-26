d9c8e575814662d630daed8fbbb89b54c33802fa
Avishai Dernis <avid29@live.com>
2020-03-26 13:04:47 -0400

    Less unknown errors

--- src/Calculator-Lib/src/ParserErrors.cpp
+++ src/Calculator-Lib/src/ParserErrors.cpp
@@ -6,15 +6,19 @@ string ParseErrorToString(ParserState error, string& input, int positon) {
 	{
 		case ParserState::CANNOT_PROCEED:
 			stringStream << "'" << input[positon]
-				<< "' cannot proceed '" << input[positon - 1] << "'";
+				<< "' cannot proceed '" << input[positon - 1] << "'.";
 			break;
 
 		case ParserState::UNPAIRED_PARENTHESIS:
 			stringStream << "Parenthesis unbalanced.";
 			break;
 
+		case ParserState::ALREADY_FLOAT:
+			stringStream << "Decimal not allowed when number is already a float.";
+			break;
+
 		case ParserState::UNKNOWN_ERROR:
-			stringStream << "Unknown error";
+			stringStream << "Unknown error.";
 			break;
 	}
 	stringStream << endl;

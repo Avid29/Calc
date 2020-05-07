73cb5031835e1573e5e6bec7e0133f5e849f7a5e
Adam Dernis <avid29@live.com>
2020-05-07 15:15:29 -0400

    Moved all tests to msft unit tests

--- tests/Calculator-Tests/Addition.cpp
+++ tests/Calculator-Tests/Addition.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "CppUnitTest.h"
 
 #include "include/Parse.h"
@@ -10,8 +9,82 @@ namespace CalculatorTests
 	TEST_CLASS(Tests)
 	{
 	public:
-		TEST_METHOD(AllTests)
+		TEST_METHOD(Add)
 		{
+			Assert::IsTrue(Run("2+5") == "7");
+		}
+
+		TEST_METHOD(Subtract)
+		{
+			Assert::IsTrue(Run("5-2") == "3");
+		}
+
+		TEST_METHOD(Multiply)
+		{
+			Assert::IsTrue(Run("4*3") == "12");
+		}
+
+		TEST_METHOD(Power)
+		{
+			Assert::IsTrue(Run("3^4") == "81");
+		}
+
+		TEST_METHOD(ImpliedMultiply)
+		{
+			Assert::IsTrue(Run("3(4+5)") == "27");
+		}
+
+		TEST_METHOD(ParenthesisDropping)
+		{
+			Assert::IsTrue(Run("(4+5)") == "9");
+		}
+
+		TEST_METHOD(DecimalMultiplyParenthesis)
+		{
+			Assert::IsTrue(Run("3(4+5)(0.1)") == "2.7");
+		}
+
+		TEST_METHOD(MoreDecimalMultiplyParenthesis)
+		{
+			Assert::IsTrue(Run("(0.1)3(4+5)") == "2.7");
+		}
+
+		TEST_METHOD(Variable)
+		{
+			Assert::IsTrue(Run("x-2") == "x-2");
+		}
+
+		TEST_METHOD(MultiplyZeroVariable)
+		{
+			Assert::IsTrue(Run("(0)3(4x+5)") == "0");
+		}
+
+		TEST_METHOD(CombineMultiplicativeTerms)
+		{
+			Assert::IsTrue(Run("2x*2x") == "4x^2");
+		}
+
+		TEST_METHOD(CombineAdditiveTerms)
+		{
+			Assert::IsTrue(Run("3x+(5x+6x+8xy)") == "14x+8xy");
+		}
+
+		TEST_METHOD(DistributiveProperty)
+		{
+			Assert::IsTrue(Run("3(x+3)") == "3x+9");
+		}
+
+		TEST_METHOD(RecursiveDistributiveProperty)
+		{
+			Assert::IsTrue(Run("(x+3)(x+3)") == "x^2+6x+9");
+		}
+
+	private:
+		string Run(string str) {
+			unique_ptr<ExpTree> tree;
+			int result = Parse(str, tree);
+			Assert::AreEqual(result, -1);
+			return tree->Simplify()->Print();
 		}
 	};
 }

8ba1523a7eebefbd2a43d5d5a0886b0979c39f65
Avishai Dernis <avid29@live.com>
2020-05-26 10:22:06 -0400

    Differentiate constants

---
+++ src/Calculator-Lib/Differentiator.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "IOperation.h"
+
+class Differentiator : public IOperation
+{
+public:
+	Differentiator(unique_ptr<VarValueNode> variable) {
+		variable_ = move(variable);
+	}
+
+	unique_ptr<ExpNode> Execute(const BOperNode&) override;
+
+	unique_ptr<ExpNode> Execute(const DiffOperNode&) override;
+
+	unique_ptr<ExpNode> Execute(const FValueNode&) override;
+
+	unique_ptr<ExpNode> Execute(const IValueNode&) override;
+
+	unique_ptr<ExpNode> Execute(const NOperNode&) override;
+
+	unique_ptr<ExpNode> Execute(const UOperNode&) override;
+
+	unique_ptr<ExpNode> Execute(const VarValueNode&) override;
+
+private:
+	unique_ptr<VarValueNode> variable_;
+};

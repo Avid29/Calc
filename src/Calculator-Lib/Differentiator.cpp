a12c27679728e9051f6e9500b92c5358435b18e5
Adam Dernis <avid29@live.com>
2020-05-28 13:58:48 -0400

    Simple exponents rule

--- src/Calculator-Lib/Differentiator.cpp
+++ src/Calculator-Lib/Differentiator.cpp
@@ -12,11 +12,20 @@
 
 unique_ptr<ExpNode> Differentiator::Execute(const BOperNode& node) {
 	// TODO: Multiply by exponent, subtract one from exponent
-	return node.Clone();
+	unique_ptr<NOperNode> mNode = make_unique<NOperNode>(Operator::MULTIPLICATION);
+	mNode->AddChild(node.GetChild(1).Clone());
+	unique_ptr<BOperNode> bNode = make_unique<BOperNode>(Operator::POWER);
+	bNode->AddChild(node.GetChild(0).Clone());
+	unique_ptr<NOperNode> aNode = make_unique<NOperNode>(Operator::ADDITION);
+	aNode->AddChild(node.GetChild(1).Clone());
+	aNode->AddChild(MakeValueNode(-1));
+	bNode->AddChild(move(aNode));
+	mNode->AddChild(move(bNode));
+	return mNode;
 }
 
 unique_ptr<ExpNode> Differentiator::Execute(const DiffOperNode& node) {
-	// TODO: Should not hit
+	// Should not hit
 	return node.Clone();
 }
 

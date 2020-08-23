9ed285ff67be51ad2431f65257919dd177ff7893
Adam Dernis <avid29@live.com>
2020-08-23 18:23:08 -0400

    UnoApp runtime component initial commit

---
+++ Calculator.Apps.UnoApp.CalculatorInterface/Class1.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace Calculator::Apps::UnoApp::CalculatorInterface;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ Calculator.Apps.UnoApp.CalculatorInterface/Class1.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace Calculator {
+    namespace Apps {
+        namespace UnoApp {
+            namespace CalculatorInterface {
+                public ref class Class1 sealed {
+
+                public:
+                    Class1();
+
+                };
+            }
+        }
+    }
+}

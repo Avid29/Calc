5fb5f2e413d59dd142b67812c964fc97d7b93191
Adam Dernis <avid29@live.com>
2020-08-23 01:05:53 -0400

    Shifted file layout an project naming to be by namespaces

---
+++ src/Clients/Consoles/GenericConsole/Generated Files/sources/CalculatorState.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CalculatorState.h"
+
+// Note: Remove this static_assert after copying these generated source files to your project.
+// This assertion exists to avoid compiling these generated source files directly.
+static_assert(false, "Do not compile generated C++/WinRT source files directly");
+
+namespace winrt::Calculator::implementation
+{
+    void CalculatorState::Clear()
+    {
+        throw hresult_not_implemented();
+    }
+    bool CalculatorState::ParseNextChar(char16_t c)
+    {
+        throw hresult_not_implemented();
+    }
+    hstring CalculatorState::Simplify()
+    {
+        throw hresult_not_implemented();
+    }
+}

16daec88216458b79feec2a547bf06b22a0b7a1e
Adam Dernis <avid29@live.com>
2020-09-24 12:07:31 -0400

    Added some PartialDiff tests

---
+++ tests/Calculator.Tests/PartialDiff.cpp
@@ -0,0 +1,51 @@
+#include "CppUnitTest.h"
+
+#include "InternalParser.h"
+#include "InternalPrinter.h"
+#include "Simplify.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorTests {
+	namespace Operators {
+		TEST_CLASS(PartialDiff)
+		{
+		public:
+			PartialDiff() {
+				printer = new InternalPrinter();
+				simlifier = new Simplifier();
+			}
+
+			~PartialDiff() {
+				delete printer;
+			}
+
+			TEST_METHOD(PowerRule)
+			{
+				Assert::IsTrue(Run("\\diff[x]{x^5}") == "5x^4");
+			}
+
+			TEST_METHOD(AdditionRule)
+			{
+				Assert::IsTrue(Run("\\diff[x]{x^2 + 2x + 3}") == "2x+2");
+			}
+
+			TEST_METHOD(ProductRule)
+			{
+				Assert::IsTrue(Run("\\diff[x]{2xy}") == "2y + 2x\\diff[x]{y}");
+			}
+
+		private:
+			string Run(string str) {
+				unique_ptr<ExpTree> tree;
+				unique_ptr<ExpTree>* treePtr = &tree;
+				Status result = Parse(str, treePtr);
+				Assert::IsFalse(result.Failed());
+				return tree->Execute(simlifier)->Print(*printer);
+			}
+
+			Simplifier* simlifier;
+			IPrinter* printer;
+		};
+	}
+}

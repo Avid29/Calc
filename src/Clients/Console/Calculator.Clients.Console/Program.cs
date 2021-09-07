bc51dcc0e07d59938e2d97f1a7851dd8a1c9d217
Adam Dernis <avid29@live.com>
2021-09-07 00:49:53 -0400

    Created C# files for all classes

---
+++ src/Clients/Console/Calculator.Clients.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using SysCon = System.Console;
+
+namespace Calculator.Clients.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SysCon.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Library/Calculator/ExpTree/Nodes/Operators/NOpers/NOperNode.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.ExpTree.Nodes.Operators
+{
+    class NOperNode
+    {
+    }
+}

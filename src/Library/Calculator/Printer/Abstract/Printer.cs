68a9e01b6a4e44bb429b63fad5d53fc61d923f27
Adam Dernis <avid29@live.com>
2021-09-07 13:46:01 -0400

    Printer abstract methods

--- src/Library/Calculator/Printer/Abstract/Printer.cs
+++ src/Library/Calculator/Printer/Abstract/Printer.cs
@@ -1,10 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Calculator.ExpTree.Nodes;
+using Calculator.ExpTree.Nodes.Collections;
+using Calculator.ExpTree.Nodes.Operators;
+using Calculator.ExpTree.Nodes.Operators.BOpers;
+using Calculator.ExpTree.Nodes.Operators.Functions;
+using Calculator.ExpTree.Nodes.Operators.NOpers;
+using Calculator.ExpTree.Nodes.Operators.UOpers;
+using Calculator.ExpTree.Nodes.Operators.UOpers.SignNode;
+using Calculator.ExpTree.Nodes.Operators.UOpers.SineNode;
+using Calculator.ExpTree.Nodes.Values;
 
-namespace Calculator.Printer.Abstract
+namespace Calculator.Printers.Abstract
 {
-    class Printer
+    public abstract class Printer
     {
+        public virtual string Print(AdditionOperNode node) => Print((NOperNode)node);
+
+        public virtual string Print(BranchNode node) => Print((ExpNode)node);
+
+        public virtual string Print(BOperNode node) => Print((OperNode)node);
+
+        public virtual string Print(DiffOperNode node) => Print((OperNode)node);
+
+        public virtual string Print(EnumerableCollectionNode node) => Print((BranchNode)node);
+
+        public abstract string Print(ExpNode node);
+
+        public virtual string Print(FloatValueNode node) => Print((ValueNode)node);
+
+        public virtual string Print(IntegralOperNode node) => Print((OperNode)node);
+
+        public virtual string Print(IntValueNode node) => Print((ValueNode)node);
+
+        public virtual string Print(MultiplicationOperNode node) => Print((NOperNode)node);
+
+        public virtual string Print(NOperNode node) => Print((OperNode)node);
+
+        public virtual string Print(OperNode node) => Print((BranchNode)node);
+
+        public virtual string Print(ParenthesisOperNode node) => Print((UOperNode)node);
+
+        public virtual string Print(PowOperNode node) => Print((BOperNode)node);
+
+        public virtual string Print(SineOperNode node) => Print((UOperNode)node);
+
+        public virtual string Print(SignOperNode node) => Print((UOperNode)node);
+
+        public virtual string Print(TensorNode node) => Print((EnumerableCollectionNode)node);
+
+        public virtual string Print(UOperNode node) => Print((OperNode)node);
+
+        public virtual string Print(VarValueNode node) => Print((ValueNode)node);
+
+        public virtual string Print(ValueNode node) => Print((ExpNode)node);
     }
 }
--- src/Library/Calculator/Printer/Default/DefaultPrinter.cs
+++ src/Library/Calculator/Printer/Default/DefaultPrinter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Calculator.Printer.Default
+namespace Calculator.Printers.Default
 {
     class DefaultPrinter
     {

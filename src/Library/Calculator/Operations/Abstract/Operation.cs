e372a4a9c8cf6ae4d32df24a529b733f5f594bc3
Adam Dernis <avid29@live.com>
2021-09-07 12:23:15 -0400

    Added methods to Operation

--- src/Library/Calculator/Operations/Abstract/Operation.cs
+++ src/Library/Calculator/Operations/Abstract/Operation.cs
@@ -1,10 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Calculator.ExpTree.Nodes;
+using Calculator.ExpTree.Nodes.Collections;
+using Calculator.ExpTree.Nodes.Operators;
+using Calculator.ExpTree.Nodes.Operators.BOpers;
+using Calculator.ExpTree.Nodes.Operators.Functions;
+using Calculator.ExpTree.Nodes.Operators.NOpers;
+using Calculator.ExpTree.Nodes.Operators.UOpers;
+using Calculator.ExpTree.Nodes.Operators.UOpers.SignNode;
+using Calculator.ExpTree.Nodes.Operators.UOpers.SineNode;
+using Calculator.ExpTree.Nodes.Values;
 
 namespace Calculator.Operations.Abstract
 {
-    class Operation
+    public abstract class Operation
     {
+        public virtual ExpNode Execute(AdditionOperNode node) => Execute((NOperNode)node);
+
+        public virtual ExpNode Execute(BranchNode node) => Execute((ExpNode)node);
+
+        public virtual ExpNode Execute(BOperNode node) => Execute((OperNode)node);
+
+        public virtual ExpNode Execute(DiffOperNode node) => Execute((OperNode)node);
+
+        public virtual ExpNode Execute(EnumerableCollectionNode node) => Execute((BranchNode)node);
+
+        public abstract ExpNode Execute(ExpNode node);
+
+        public virtual ExpNode Execute(FloatValueNode node) => Execute((ValueNode)node);
+
+        public virtual ExpNode Execute(IntegralOperNode node) => Execute((OperNode)node);
+
+        public virtual ExpNode Execute(IntValueNode node) => Execute((ValueNode)node);
+
+        public virtual ExpNode Execute(MultiplicationOperNode node) => Execute((NOperNode)node);
+
+        public virtual ExpNode Execute(NOperNode node) => Execute((OperNode)node);
+
+        public virtual ExpNode Execute(OperNode node) => Execute((BranchNode)node);
+
+        public virtual ExpNode Execute(ParenthesisOperNode node) => Execute((UOperNode)node);
+
+        public virtual ExpNode Execute(PowOperNode node) => Execute((BOperNode)node);
+
+        public virtual ExpNode Execute(SineOperNode node) => Execute((UOperNode)node);
+
+        public virtual ExpNode Execute(SignOperNode node) => Execute((UOperNode)node);
+
+        public virtual ExpNode Execute(TensorNode node) => Execute((EnumerableCollectionNode)node);
+
+        public virtual ExpNode Execute(UOperNode node) => Execute((OperNode)node);
+
+        public virtual ExpNode Execute(VarValueNode node) => Execute((ValueNode)node);
+
+        public virtual ExpNode Execute(ValueNode node) => Execute((ExpNode)node);
     }
 }

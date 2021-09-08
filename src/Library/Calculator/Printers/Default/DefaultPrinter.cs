46f660df618a9c193da3de2ba67e9c6b3960a838
Adam Dernis <avid29@live.com>
2021-09-07 22:19:22 -0400

    Addition, Mutliplication, DiffOper, IntegralOper and SignOper printing

--- src/Library/Calculator/Printers/Default/DefaultPrinter.cs
+++ src/Library/Calculator/Printers/Default/DefaultPrinter.cs
@@ -1,5 +1,8 @@
 ﻿using Calculator.ExpressionTree.Nodes;
 using Calculator.ExpressionTree.Nodes.Operators.BOpers;
+using Calculator.ExpressionTree.Nodes.Operators.Functions;
+using Calculator.ExpressionTree.Nodes.Operators.NOpers;
+using Calculator.ExpressionTree.Nodes.Operators.UOpers.SignNode;
 using Calculator.ExpressionTree.Nodes.Values;
 using Calculator.Printers.Abstract;
 
@@ -7,9 +10,33 @@ namespace Calculator.Printers.Default
 {
     public class DefaultPrinter : Printer
     {
+        public override string Print(AdditionOperNode node)
+        {
+            string cache = "";
+            for (int i = 0; i < node.ChildCount; i++)
+            {
+                var child = node.GetChild(i);
+                bool isSignChild = child is SignOperNode;
+                if (!(i == 0 || isSignChild))
+                {
+                    // Don't add if isSignChild
+                    // or if first iteration
+                    cache += "+";
+                }
+
+                cache += child.Print(this);
+            }
+            return cache;
+        }
+
         public override string Print(ExpNode node)
         {
             return "";
+        }
+
+        public override string Print(DiffOperNode node)
+        {
+            return $"\\diff[{node.Variable.Print(this)}]{{{node.Child.Print(this)}}}";
         }
 
         public override string Print(FloatValueNode node)
@@ -17,14 +44,45 @@ namespace Calculator.Printers.Default
             return $"{node.Value}";
         }
 
+        public override string Print(IntegralOperNode node)
+        {
+            if (node.IsDeterminate)
+                return $"\\int[{node.Variable.Print(this)}]{{{node.Child.Print(this)}}}";
+            else
+                return $"\\int[{node.Variable.Print(this)}, {node.LowerBound.Print(this)}, {node.UpperBound.Print(this)}]{{{node.Child.Print(this)}}}";
+        }
+
         public override string Print(IntValueNode node)
         {
             return $"{node.Value}";
         }
 
+        public override string Print(MultiplicationOperNode node)
+        {
+            string cache = "";
+            for (int i = 0; i < node.ChildCount; i++)
+            {
+                cache += node.GetChild(i).Print(this);
+            }
+            return cache;
+        }
+
         public override string Print(PowOperNode node)
         {
             return $"{node.LeftChild.Print(this)}^{node.RightChild.Print(this)}";
+        }
+
+        public override string Print(SignOperNode node)
+        {
+            switch (node.Sign)
+            {
+                case Sign.POSITIVE:
+                    return $"+{node.Child.Print(this)}";
+                case Sign.NEGATIVE:
+                    return $"-{node.Child.Print(this)}";
+                default:
+                    return node.Child.Print(this);
+            }
         }
 
         public override string Print(VarValueNode node)

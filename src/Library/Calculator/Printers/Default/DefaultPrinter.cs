cbad0e26a0120f312ed6de8314d5cd52e6d836fb
Adam Dernis <avid29@live.com>
2021-09-07 22:00:54 -0400

    PowNode printing

--- src/Library/Calculator/Printers/Default/DefaultPrinter.cs
+++ src/Library/Calculator/Printers/Default/DefaultPrinter.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Calculator.ExpressionTree.Nodes;
+using Calculator.ExpressionTree.Nodes.Operators.BOpers;
+using Calculator.Printers.Abstract;
 
 namespace Calculator.Printers.Default
 {
-    class DefaultPrinter
+    public class DefaultPrinter : Printer
     {
+        public override string Print(ExpNode node)
+        {
+            return "";
+        }
+
+        public override string Print(PowOperNode node)
+        {
+            return $"{node.LeftChild.Print(this)} ^ {node.RightChild.Print(this)}";
+        }
     }
 }

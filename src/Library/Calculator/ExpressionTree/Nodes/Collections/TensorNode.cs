1e499dc7b94cf0983f0fa0ffeff3e5f4ac84987d
Adam Dernis <avid29@live.com>
2021-09-15 21:43:04 -0400

    Fixed issues with TensorNode GetChild

--- src/Library/Calculator/ExpressionTree/Nodes/Collections/TensorNode.cs
+++ src/Library/Calculator/ExpressionTree/Nodes/Collections/TensorNode.cs
@@ -19,7 +19,7 @@ namespace Calculator.ExpressionTree.Nodes.Collections
         /// Initializes a new instance of the <see cref="TensorNode"/> class.
         /// </summary>
         /// <param name="sizes">The size of each dimension in the <see cref="TensorNode"/>.</param>
-        public TensorNode(int[] sizes)
+        public TensorNode(params int[] sizes)
         {
             _dimensionCount = sizes.Length;
             _sizes = sizes;
@@ -93,20 +93,20 @@ namespace Calculator.ExpressionTree.Nodes.Collections
         /// </summary>
         /// <param name="index">The index in the dimension</param>
         /// <returns>The child at the n dimensional index.</returns>
-        public ExpNode GetChild(params int[] index)
+        public ExpNode GetChildD(params int[] index)
         {
             if (index.Length != _dimensionCount)
                 throw new ArgumentOutOfRangeException(nameof(index), $"{index.Length} must match the dimensions of the {nameof(TensorNode)}.");
 
             int realIndex = 0; // The index for the flat tensor.
             int realSize = 1; // The number of indecies to increment for the current dimension.
-            for (int dim = _dimensionCount-1; dim >= 0; dim--)
+            for (int dim = DimensionCount - 1; dim >= 0; dim--)
             {
                 realIndex += index[dim] * realSize;
-                realSize *= _sizes[dim];
+                realSize *= _sizes[DimensionCount - dim - 1];
             }
 
-            return base.GetChild(realIndex);
+            return GetChild(realIndex);
         }
 
         /// <inheritdoc/>

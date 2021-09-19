178416c557c425009f84b15abd32a6a2b93587d8
Adam Dernis <avid29@live.com>
2021-09-19 17:34:41 -0400

    Created VectorProductOperNode and enum VectorProductMethod

---
+++ src/Library/Calculator/ExpressionTree/Nodes/Operators/Functions/VectorProduct/VectorProductMethod.cs
@@ -0,0 +1,20 @@
+﻿// Adam Dernis © 2021
+
+namespace Calculator.ExpressionTree.Nodes.Operators.Functions.VectorProduct
+{
+    /// <summary>
+    /// The method used for vector products.
+    /// </summary>
+    public enum VectorProductMethod
+    {
+        /// <summary>
+        /// Find the dot product.
+        /// </summary>
+        DOT,
+
+        /// <summary>
+        /// Find the cross product.
+        /// </summary>
+        CROSS,
+    }
+}
---
+++ src/Library/Calculator/ExpressionTree/Nodes/Operators/Functions/VectorProduct/VectorProductOperNode.cs
@@ -0,0 +1,46 @@
+﻿// Adam Dernis © 2021
+
+using Calculator.Operations.Abstract;
+using Calculator.Printers.Abstract;
+
+namespace Calculator.ExpressionTree.Nodes.Operators.Functions.VectorProduct
+{
+    /// <summary>
+    /// An <see cref="ExpNode"/> that finds vector products.
+    /// </summary>
+    public class VectorProductOperNode : BOperNode
+    {
+        private VectorProductMethod _productMethod;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VectorProductOperNode"/> class.
+        /// </summary>
+        /// <param name="productMethod">The method for vector multiplication.</param>
+        public VectorProductOperNode(VectorProductMethod productMethod)
+        {
+            _productMethod = productMethod;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VectorProductOperNode"/> class.
+        /// </summary>
+        /// <param name="node">The <see cref="VectorProductOperNode"/> to clone.</param>
+        public VectorProductOperNode(VectorProductOperNode node)
+            : base(node)
+        {
+            _productMethod = node._productMethod;
+        }
+
+        /// <inheritdoc/>
+        public override Priority Priority => Priority.MULTIPLICATION;
+
+        /// <inheritdoc/>
+        public override ExpNode Clone() => new VectorProductOperNode(this);
+
+        /// <inheritdoc/>
+        public override ExpNode Execute(Operation operation) => operation.Execute(this);
+
+        /// <inheritdoc/>
+        public override string Print(Printer printer) => printer.Print(this);
+    }
+}

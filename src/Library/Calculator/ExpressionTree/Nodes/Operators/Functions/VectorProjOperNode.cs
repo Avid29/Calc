a750744eac0f0f793e407395cdff80d2c26a24e8
Adam Dernis <avid29@live.com>
2021-09-19 15:39:37 -0400

    Created VectorProjOperNode

---
+++ src/Library/Calculator/ExpressionTree/Nodes/Operators/Functions/VectorProjOperNode.cs
@@ -0,0 +1,39 @@
+﻿// Adam Dernis © 2021
+
+using Calculator.ExpressionTree.Nodes.Collections;
+using Calculator.Operations.Abstract;
+using Calculator.Printers.Abstract;
+
+namespace Calculator.ExpressionTree.Nodes.Operators.Functions
+{
+    /// <summary>
+    /// An <see cref="ExpNode"/> that applies Vector projection on two (2) <see cref="TensorNode"/>.
+    /// </summary>
+    public class VectorProjOperNode : BOperNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VectorProjOperNode"/> class.
+        /// </summary>
+        public VectorProjOperNode()
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VectorProjOperNode"/> class as a clone of <paramref name="node"/>.
+        /// </summary>
+        /// <param name="node">The <see cref="VectorProjOperNode"/> to clone.</param>
+        public VectorProjOperNode(VectorProjOperNode node)
+            : base(node) { }
+
+        /// <inheritdoc/>
+        public override Priority Priority => Priority.OVERRIDE;
+
+        /// <inheritdoc/>
+        public override ExpNode Clone() => new VectorProjOperNode(this);
+
+        /// <inheritdoc/>
+        public override ExpNode Execute(Operation operation) => operation.Execute(this);
+
+        /// <inheritdoc/>
+        public override string Print(Printer printer) => printer.Print(this);
+    }
+}
